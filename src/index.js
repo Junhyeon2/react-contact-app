3cb6472ddb643154acc2dbe539e1d3db97e0f637
Junhyeon2 <junhyeon47@gmail.com>
2018-10-29 22:04:41 +0900

    input 상태 관리하기.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// 특정 아이디를 가지고 있는 DOM을 가져와서 리액트 컴포넌트를 그려준다.
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
